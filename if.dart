5750915f64447749d643ace027c3c30350d19f7e
Sarayu2203 <sarayushiramshetti22@gmail.com>
2025-03-12 10:30:00 +0530

    Add files via upload

---
+++ if.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main(){
+int a = int.parse(stdin.readLineSync()!);
+int b = int.parse(stdin.readLineSync()!);
+int c = int.parse(stdin.readLineSync()!);
+if({a} case {b}){
+print(a is greater)
+}
+else if({b} case {c}){
+print(b is greater)
+}
+else{
+print(c is greater)
+}
+
+
+
